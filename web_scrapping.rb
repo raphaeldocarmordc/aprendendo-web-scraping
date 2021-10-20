c8b40bb122e32451984527bb134a201c5ed84240
raphaeldocarmordc <raphaeldocarmo.rdc@gmail.com>
2021-10-20 11:28:03 -0300

    aprendendo web scraping

--- web_scrapping.rb
+++ web_scrapping.rb
@@ -4,10 +4,25 @@ require 'byebug'
 
 class MetroSP
   def initialize
-    url = 'http://www.metro.sp.gov.br/Sistemas/direto-do-metro-via4/MetroStatusLinha/mobile/smartPhone/diretoDoMetro.aspx'
+    url = "http://www.metro.sp.gov.br/Sistemas/direto-do-metro-via4/MetroStatusLinha/mobile/smartPhone/diretoDoMetro.aspx"
     html = URI.open(url)
     doc = Nokogiri::HTML(html)
-    titulo = doc.parse.title
-    #Nokogiri::HTML.parse(open('')).title
+    titulo = doc#parse
+
+    linhas = doc.css('div.status-linhas li')
+
+    nome_linha = linhas.search('p strong').map do |linha|
+      linha.content
+    end
+    pp nome_linha
+
+    
+   # linhas.xpath('//div//li').each do |infos|
+    #  nome_linha = linhas.at('.align-left').text
+    #end
+   # puts nome_linha
+
   end
 end
+
+MetroSP.new
