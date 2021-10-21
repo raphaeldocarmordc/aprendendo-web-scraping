cb23bd217b5eac10ecea929412440356a7aa93fc
raphaeldocarmordc <raphaeldocarmo.rdc@gmail.com>
2021-10-21 19:06:21 -0300

    aprendendo web scraping

--- web_scrapping.rb
+++ web_scrapping.rb
@@ -3,22 +3,24 @@ require 'open-uri'
 
 class MetroSp
 
-  attr_reader :linhas, :status, :nome_linha, :status_linha, :hash_das_informacoes, :url, :html, :doc
+  attr_reader :linhas, :i, :escolha_do_usuario, :nome_linha, :status_linha, :string_para_verificacao, :hash_das_informacoes, :url, :html, :doc, :conteudo_linhas
 
   def initialize
     @url = "http://www.metro.sp.gov.br/Sistemas/direto-do-metro-via4/MetroStatusLinha/mobile/smartPhone/diretoDoMetro.aspx"
     @html = URI.open(url)
     @doc = Nokogiri::HTML(html)
+    @conteudo_linhas = doc.css('div.status-linhas li')
   end
 
   def web
     get_name
     get_status
     hash_zipado
+    escolher_estacao
+    conferir_escolha
   end
 
   def get_name
-    #@linhas = doc.css('div.status-linhas li')
     #@nome_linha = @linhas.search('p strong').map do |linha|
     #  linha.content
     #end
@@ -26,24 +28,48 @@ class MetroSp
     @nome_linha = linhas.map do |linha|
       linha.content
     end
-    pp nome_linha
   end
 
   def get_status
-    #status = doc.css('div.status-linhas li')
-    #status_linha = status.search('p a').map do |operacao|
-    #  operacao.content
-    #end
-    @status = doc.xpath("//li//div//p//a")
-    @status_linha = status.map do |operacao|
-      operacao.content
+    status_linhas = conteudo_linhas.search('li a')
+    status_linhas.map do |operacao|
+      operacao.content.strip.tr("\t/\r/\n", "") #método strip pega todos os caractéres antes da vírgula como string e substitui pelo que esta depois da vírgula como string
     end
-    pp status_linha
   end
 
   def hash_zipado
-    @hash_das_informacoes = nome_linha.zip(status_linha).to_h
-    pp hash_das_informacoes
+    @hash_das_informacoes = nome_linha.zip(get_status).to_h.map{|key, value| {name: key, status: value}}
+  end
+
+  def escolher_estacao
+    puts "\nDentre as #{nome_linha.size} linhas do Metrô de SP,"
+    puts "se tem as opções: #{nome_linha}."
+    puts "Você deseja consultar o status de qual delas?"
+    puts "Digite o nome ou o número da linha escolhida, por gentileza: "
+    escolha_do_usuario = gets.chomp.downcase.to_s
+    @string_para_verificacao = escolha_do_usuario.strip.tr(" ", "")
+  end
+
+  def conferir_escolha
+    case string_para_verificacao
+    when "linha1", "1"
+      puts hash_das_informacoes[0].values
+    when "linha2", "2"
+      puts hash_das_informacoes[1].values
+    when "linha3", "3"
+      puts hash_das_informacoes[2].values
+    when "linha15", "15"
+      puts hash_das_informacoes[3].values
+    when "linha4", "4"
+      puts hash_das_informacoes[4].values
+    else
+      puts "\nInformação não encontrada"
+    end
+
+  #  hash_das_informacoes.each do |hash|
+  #    hash.select{|k, v| v == escolha_do_usuario}
+  #  end
+
   end
 end
 
